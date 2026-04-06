03c59824ed1ea4e39562a24134314f015ca88397
sans690 <scsmith7255@gmail.com>
2026-04-06 13:27:18 -0400

    Added Dijkstra's description

--- obj/Debug/net10.0/Algorithms.AssemblyInfo.cs
+++ obj/Debug/net10.0/Algorithms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e8d4fcdaa5e452bc3d18b004afb144b0b7ada56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3965269aa07cb3f139ed4f11b56383715f25f4dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
